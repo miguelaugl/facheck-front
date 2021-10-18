19d6648e3e9a57d95135b0711f7b112c60d44d24
Miguel <miguelaugl@outlook.com>
2021-10-18 12:46:06 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from '@/domain/models'
+
+export interface Authentication {
+  auth: (params: Authentication.Params) => Promise<Authentication.Result>
+}
+
+export namespace Authentication {
+  export type Params = {
+    email: string
+    password: string
+  }
+
+  export type Result = AccountModel
+}
