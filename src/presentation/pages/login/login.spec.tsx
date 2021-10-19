dc13dea8b8fe3fb686663fe22eb7fee8daccf783
Miguel <miguelaugl@outlook.com>
2021-10-19 14:08:14 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,9 +2,13 @@ import { render, screen } from '@testing-library/react'
 
 import { Login } from './login'
 
+const makeSut = (): void => {
+  render(<Login />)
+}
+
 describe('Login Component', () => {
   it('should start with correct initial state', () => {
-    render(<Login />)
+    makeSut()
     expect(screen.getByTestId('submit')).toBeDisabled()
     expect(screen.getByTestId('email')).not.toHaveValue()
     expect(screen.getByTestId('password')).not.toHaveValue()
