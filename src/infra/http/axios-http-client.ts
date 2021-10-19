60f867b05c383341d855dee5492a6405e5aa0e0e
Miguel <miguelaugl@outlook.com>
2021-10-19 18:32:19 -0300

    feat: ensure AxiosHttpClient returns correct response on axios

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -4,12 +4,15 @@ import { HttpClient, HttpRequest, HttpResponse } from '@/data/protocols'
 
 export class AxiosHttpClient implements HttpClient {
   async request (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await axios.request({
+    const axiosResponse = await axios.request({
       url: httpRequest.url,
       data: httpRequest.body,
       headers: httpRequest.headers,
       method: httpRequest.method,
     })
-    return null
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    }
   }
 }
