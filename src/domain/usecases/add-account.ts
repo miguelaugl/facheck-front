4d1324b614127f90a787291c5dc4d3a20d5b53e5
Miguel <miguelaugl@outlook.com>
2021-10-19 23:29:37 -0300

    feat: add AddAccount

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,19 @@
+import { AccountModel } from '@/domain/models'
+
+export interface AddAccount {
+  add: (account: AddAccount.Params) => Promise<AddAccount.Result>
+}
+
+export namespace AddAccount {
+  export type Params = {
+    name: string
+    email: string
+    password: string
+    ra: string
+    course: string
+    cpf: string
+    role?: string
+  }
+
+  export type Result = AccountModel
+}
