f768df2ef06db167c92ade1cca6b9af561e06496
Miguel <miguelaugl@outlook.com>
2021-10-19 23:10:15 -0300

    feat: ensure CurrentAccountAdapter calls LocalStorageAdapter.set with correct values

---
+++ src/main/adapters/current-account-adapter.ts
@@ -0,0 +1,6 @@
+import { AccountModel } from '@/domain/models'
+import { makeLocalStorageAdapter } from '@/main/factories/cache'
+
+export const setCurrentAccountAdapter = (account: AccountModel): void => {
+  makeLocalStorageAdapter().set('account', account)
+}
--- src/main/router/router.tsx
+++ src/main/router/router.tsx
@@ -2,17 +2,25 @@ import '@/presentation/config/yupLocale'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import { setCurrentAccountAdapter } from '@/main/adapters'
 import { theme } from '@/main/config/theme'
 import { makeLogin } from '@/main/factories/pages'
+import { ApiContext } from '@/presentation/contexts'
 
 export const Router = (): JSX.Element => {
   return (
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Switch>
-          <Route path='/login' exact component={makeLogin} />
-        </Switch>
-      </BrowserRouter>
-    </ChakraProvider>
+    <ApiContext.Provider
+      value={{
+        setCurrentAccount: setCurrentAccountAdapter,
+      }}
+    >
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/login' exact component={makeLogin} />
+          </Switch>
+        </BrowserRouter>
+      </ChakraProvider>
+    </ApiContext.Provider>
   )
 }
