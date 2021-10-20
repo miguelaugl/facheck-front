d11ed0281a56783e29edc5fe976d2fc68ae7a6e8
Miguel <miguelaugl@outlook.com>
2021-10-19 23:54:10 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpClient returns 400, 500, 404

--- src/data/usecases/remote-add-account.spec.ts
+++ src/data/usecases/remote-add-account.spec.ts
@@ -2,7 +2,7 @@ import faker from 'faker'
 
 import { HttpStatusCode } from '@/data/protocols'
 import { HttpClientSpy } from '@/data/tests'
-import { EmailInUseError } from '@/domain/errors'
+import { EmailInUseError, UnexpectedError } from '@/domain/errors'
 import { mockAddAccountParams } from '@/domain/tests'
 
 import { RemoteAddAccount } from './remote-add-account'
@@ -39,5 +39,32 @@ describe('RemoteAddAccount Usecase', () => {
     }
     const promise = sut.add(mockAddAccountParams())
     await expect(promise).rejects.toThrow(new EmailInUseError())
+  })
+
+  it('should throw UnexpectedError if HttpClient returns 400', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.BAD_REQUEST,
+    }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should throw UnexpectedError if HttpClient returns 500', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.INTERNAL_SERVER_ERROR,
+    }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should throw UnexpectedError if HttpClient returns 404', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.NOT_FOUND,
+    }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
