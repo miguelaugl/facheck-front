b22af482dae8607e5adc2eadd49176599077cf3b
Miguel <miguelaugl@outlook.com>
2021-10-20 20:41:12 -0300

    feat: ensure CurrentAccountAdapter calls LocalStorageAdapter.get with correct key

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -4,3 +4,7 @@ import { makeLocalStorageAdapter } from '@/main/factories/cache'
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
   makeLocalStorageAdapter().set('account', account)
 }
+
+export const getCurrentAccountAdapter = (): AccountModel => {
+  return makeLocalStorageAdapter().get('account')
+}
