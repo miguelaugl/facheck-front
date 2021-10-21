1442241569942cda399b5ccc1aef92b31a55fe80
Miguel <miguelaugl@outlook.com>
2021-10-21 20:17:32 -0300

    feat: ensure RemoteLoadMonitorings throws UnexpectedError if HttpClient returns 500

--- src/data/usecases/remote-load-monitorings.ts
+++ src/data/usecases/remote-load-monitorings.ts
@@ -1,14 +1,18 @@
-import { HttpClient } from '@/data/protocols'
+import { HttpClient, HttpStatusCode } from '@/data/protocols'
+import { UnexpectedError } from '@/domain/errors'
 import { LoadMonitorings } from '@/domain/usecases'
 
 export class RemoteLoadMonitorings implements LoadMonitorings {
   constructor(private readonly url: string, private readonly httpClient: HttpClient<LoadMonitorings.Result>) {}
 
   async load(): Promise<LoadMonitorings.Result> {
-    await this.httpClient.request({
+    const httpResponse = await this.httpClient.request({
       url: this.url,
       method: 'GET',
     })
+    if (httpResponse.statusCode === HttpStatusCode.INTERNAL_SERVER_ERROR) {
+      throw new UnexpectedError()
+    }
     return null
   }
 }
