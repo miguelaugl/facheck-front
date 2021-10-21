f2fd6725ad0611440a1d1fa096eb2b11b590c360
Miguel <miguelaugl@outlook.com>
2021-10-21 20:50:16 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/home/home.spec.tsx
+++ src/presentation/pages/home/home.spec.tsx
@@ -8,23 +8,34 @@ import { LoadMonitoringsSpy } from '@/presentation/tests'
 
 import { Home } from './home'
 
+type SutTypes = {
+  loadMonitoringsSpy: LoadMonitoringsSpy
+}
+
+const history = createMemoryHistory({ initialEntries: ['/'] })
+const makeSut = (): SutTypes => {
+  const loadMonitoringsSpy = new LoadMonitoringsSpy()
+  render(
+    <RoutesContext.Provider value={{ routes: [{ path: '/', name: 'Dashboard' }] }}>
+      <ApiContext.Provider
+        value={{
+          getCurrentAccount: () => mockAccountModel(),
+        }}
+      >
+        <Router history={history}>
+          <Home loadMonitorings={loadMonitoringsSpy} />
+        </Router>
+      </ApiContext.Provider>
+    </RoutesContext.Provider>,
+  )
+  return {
+    loadMonitoringsSpy,
+  }
+}
+
 describe('Home Component', () => {
   it('should call LoadMonitorings', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-    const loadMonitoringsSpy = new LoadMonitoringsSpy()
-    render(
-      <RoutesContext.Provider value={{ routes: [{ path: '/', name: 'Dashboard' }] }}>
-        <ApiContext.Provider
-          value={{
-            getCurrentAccount: () => mockAccountModel(),
-          }}
-        >
-          <Router history={history}>
-            <Home loadMonitorings={loadMonitoringsSpy} />
-          </Router>
-        </ApiContext.Provider>
-      </RoutesContext.Provider>,
-    )
+    const { loadMonitoringsSpy } = makeSut()
     expect(loadMonitoringsSpy.callsCount).toBe(1)
   })
 })
