5ab11d1db13924f0b5af5f1c9a05947fd2e34cae
Miguel <miguelaugl@outlook.com>
2021-10-21 02:17:46 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,22 +1,37 @@
 import { render, screen } from '@testing-library/react'
 import { createMemoryHistory } from 'history'
+import { ComponentType } from 'react'
 import { Router } from 'react-router-dom'
 
+import { AccountModel } from '@/domain/models'
+import { mockAccountModel } from '@/domain/tests'
 import { ApiContext } from '@/presentation/contexts'
 
 import { PrivateRoute } from './private-route'
 
+type SutTypes = {
+  getCurrentAccountMock: () => AccountModel
+}
+
+const history = createMemoryHistory()
+const makeSut = (component: ComponentType<any>): SutTypes => {
+  const getCurrentAccountMock = (): AccountModel => mockAccountModel()
+  render(
+    <ApiContext.Provider value={{ getCurrentAccount: getCurrentAccountMock }}>
+      <Router history={history}>
+        <PrivateRoute component={component} />
+      </Router>
+    </ApiContext.Provider>,
+  )
+  return {
+    getCurrentAccountMock,
+  }
+}
+
 describe('PrivateRoute Component', () => {
   it('should render Private Component', () => {
-    const history = createMemoryHistory()
     const PrivateComponent = (): JSX.Element => <>Private</>
-    render(
-      <ApiContext.Provider value={{ getCurrentAccount: () => ({ accessToken: 'any_token', name: 'any_name' }) }}>
-        <Router history={history}>
-          <PrivateRoute component={PrivateComponent} />
-        </Router>
-      </ApiContext.Provider>,
-    )
+    makeSut(PrivateComponent)
     expect(screen.getByText('Private')).toBeInTheDocument()
   })
 })
