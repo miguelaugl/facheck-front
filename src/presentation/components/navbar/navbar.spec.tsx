04f83b5333b55e899cad59f5b6fb9f4fefd205dd
Miguel <miguelaugl@outlook.com>
2021-10-21 02:43:47 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/navbar/navbar.spec.tsx
+++ src/presentation/components/navbar/navbar.spec.tsx
@@ -2,24 +2,36 @@ import { render, screen } from '@testing-library/react'
 import { createMemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 
+import { AccountModel } from '@/domain/models'
 import { mockAccountModel } from '@/domain/tests'
 import { ApiContext, RoutesContext } from '@/presentation/contexts'
 
 import { Navbar } from './navbar'
 
+type SutTypes = {
+  account: AccountModel
+}
+
+const history = createMemoryHistory({ initialEntries: ['/navbar'] })
+const makeSut = (): SutTypes => {
+  const account = mockAccountModel()
+  render(
+    <RoutesContext.Provider value={{ routes: [{ path: '/navbar', name: 'Navbar' }] }}>
+      <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+        <Router history={history}>
+          <Navbar />
+        </Router>
+      </ApiContext.Provider>
+    </RoutesContext.Provider>,
+  )
+  return {
+    account,
+  }
+}
+
 describe('Navbar Component', () => {
   it('should present correct name', () => {
-    const history = createMemoryHistory({ initialEntries: ['/navbar'] })
-    const account = mockAccountModel()
-    render(
-      <RoutesContext.Provider value={{ routes: [{ path: '/navbar', name: 'Navbar' }] }}>
-        <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
-          <Router history={history}>
-            <Navbar />
-          </Router>
-        </ApiContext.Provider>
-      </RoutesContext.Provider>,
-    )
+    const { account } = makeSut()
     expect(screen.getByTestId('username')).toHaveTextContent(account.name)
   })
 })
