567082304d0fd45bab006af1b02fd057ce93079c
Miguel <miguelaugl@outlook.com>
2021-10-21 20:25:55 -0300

    feat: ensure RemoteLoadMonitorings returns a list of Monitorings if HttpClient returns 200

--- src/data/usecases/remote-load-monitorings.ts
+++ src/data/usecases/remote-load-monitorings.ts
@@ -16,6 +16,6 @@ export class RemoteLoadMonitorings implements LoadMonitorings {
     if (httpResponse.statusCode === HttpStatusCode.FORBIDDEN) {
       throw new AccessDeniedError()
     }
-    return null
+    return httpResponse.body
   }
 }
