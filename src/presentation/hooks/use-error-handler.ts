44e640f45ced47ede4068b42f26b963fe9feb664
Miguel <miguelaugl@outlook.com>
2021-10-22 00:41:52 -0300

    feat: ensure Home renders error on UnexpectedError

---
+++ src/presentation/hooks/use-error-handler.ts
@@ -0,0 +1,17 @@
+import { AccessDeniedError } from '@/domain/errors'
+
+import { useLogout } from './use-logout'
+
+type CallbackType = (error: Error) => void
+type ReturnType = CallbackType
+
+export const useErrorHandler = (callback: CallbackType): ReturnType => {
+  const logout = useLogout()
+  return (error: Error): void => {
+    if (error instanceof AccessDeniedError) {
+      logout()
+      return
+    }
+    callback(error)
+  }
+}
--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -2,6 +2,7 @@ import { Flex, Text } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 
 import { LoadMonitorings } from '@/domain/usecases'
+import { useErrorHandler } from '@/presentation/hooks'
 import { AdminLayout } from '@/presentation/layouts'
 
 import { MonitoringItem } from './monitoring-item/monitoring-item'
@@ -11,23 +12,36 @@ type Props = {
 }
 
 export const Home = ({ loadMonitorings }: Props): JSX.Element => {
-  const [monitorings, setMonitorings] = useState([] as LoadMonitorings.Result)
+  const handleError = useErrorHandler((error) => {
+    setState((prevState) => ({ ...prevState, error: error.message }))
+  })
+  const [state, setState] = useState({
+    monitorings: [] as LoadMonitorings.Result,
+    error: '',
+  })
   useEffect(() => {
     loadMonitorings
       .load()
-      .then(setMonitorings)
-      .catch(() => {})
+      .then((monitorings) => setState((prevState) => ({ ...prevState, monitorings })))
+      .catch(handleError)
   }, [])
   return (
     <AdminLayout>
       <Text fontSize='2xl' fontWeight='medium' mb='4'>
         Monitorias
       </Text>
-      <Flex as='ul' data-testid='monitoring-list'>
-        {monitorings.map((monitoring) => (
-          <MonitoringItem key={monitoring.id} monitoring={monitoring} />
-        ))}
-      </Flex>
+      {!!state.error && (
+        <Text color='red.500' data-testid='error'>
+          {state.error}
+        </Text>
+      )}
+      {!state.error && (
+        <Flex as='ul' data-testid='monitoring-list'>
+          {state.monitorings.map((monitoring) => (
+            <MonitoringItem key={monitoring.id} monitoring={monitoring} />
+          ))}
+        </Flex>
+      )}
     </AdminLayout>
   )
 }
