908fab290a03c3b0e729b87b4734c7fed3ab1113
Miguel <miguelaugl@outlook.com>
2021-10-23 00:12:26 -0300

    feat: add WeekdaySelector

---
+++ src/presentation/components/weekday-selector/weekday-selector.tsx
@@ -0,0 +1,95 @@
+import { HStack, Button, Tooltip, FormLabel, FormControl, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
+import { MouseEvent } from 'react'
+
+import { Weekday } from '@/domain/models'
+
+type Option = {
+  label: string
+  fullLabel: string
+  value: Weekday
+}
+
+const options: Option[] = [
+  {
+    label: 'D',
+    fullLabel: 'Domingo',
+    value: Weekday.SUNDAY,
+  },
+  {
+    label: 'S',
+    fullLabel: 'Segunda',
+    value: Weekday.MONDAY,
+  },
+  {
+    label: 'T',
+    fullLabel: 'Terça',
+    value: Weekday.TUESDAY,
+  },
+  {
+    label: 'Q',
+    fullLabel: 'Quarta',
+    value: Weekday.WEDNESDAY,
+  },
+  {
+    label: 'Q',
+    fullLabel: 'Quinta',
+    value: Weekday.THURSDAY,
+  },
+  {
+    label: 'S',
+    fullLabel: 'Sexta',
+    value: Weekday.FRIDAY,
+  },
+  {
+    label: 'S',
+    fullLabel: 'Sábado',
+    value: Weekday.SATURDAY,
+  },
+]
+
+export type WeekdaySelectorProps = {
+  /**
+   * defauls to Dia da semana:
+   */
+  label?: string
+  /**
+   * defauls to Inválido
+   */
+  errorMessage?: string
+  helperText?: string
+  isDisabled?: boolean
+  isInvalid?: boolean
+  value: Weekday
+  onClick: (e: MouseEvent<HTMLButtonElement>, value: Weekday) => void
+}
+
+export const WeekdaySelector = ({
+  label = 'Dia da semana:',
+  value,
+  errorMessage,
+  isDisabled,
+  isInvalid,
+  helperText,
+  onClick,
+}: WeekdaySelectorProps): JSX.Element => {
+  return (
+    <FormControl isDisabled={isDisabled} isInvalid={isInvalid}>
+      <FormLabel>{label}</FormLabel>
+      <HStack spacing='3'>
+        {options.map((option) => (
+          <Tooltip label={option.fullLabel} key={option.value} placement='top'>
+            <Button
+              variant={option.value === value ? 'solid' : 'outline'}
+              flexGrow={1}
+              onClick={(e: MouseEvent<HTMLButtonElement>) => onClick(e, option.value)}
+            >
+              {option.label}
+            </Button>
+          </Tooltip>
+        ))}
+      </HStack>
+      <FormHelperText>{helperText}</FormHelperText>
+      <FormErrorMessage>{errorMessage}</FormErrorMessage>
+    </FormControl>
+  )
+}
