990eefdf9afbe2746acf1bf40c08c159276afab2
Miguel <miguelaugl@outlook.com>
2021-10-23 01:53:25 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpClient returns 400, 500 or 404

--- src/data/usecases/remote-add-monitoring.spec.ts
+++ src/data/usecases/remote-add-monitoring.spec.ts
@@ -1,6 +1,8 @@
 import faker from 'faker'
 
+import { HttpStatusCode } from '@/data/protocols'
 import { HttpClientSpy } from '@/data/tests'
+import { UnexpectedError } from '@/domain/errors'
 import { mockAddMonitoringParams } from '@/domain/tests'
 
 import { RemoteAddMonitoring } from './remote-add-monitoring'
@@ -28,5 +30,32 @@ describe('AddMonitoring Usecase', () => {
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.method).toBe('POST')
     expect(httpClientSpy.body).toEqual(addMonitoringParams)
+  })
+
+  it('should throw UnexpectedError if HttpClient returns 400', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.BAD_REQUEST,
+    }
+    const promise = sut.add(mockAddMonitoringParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should throw UnexpectedError if HttpClient returns 500', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.INTERNAL_SERVER_ERROR,
+    }
+    const promise = sut.add(mockAddMonitoringParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should throw UnexpectedError if HttpClient returns 404', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.NOT_FOUND,
+    }
+    const promise = sut.add(mockAddMonitoringParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
