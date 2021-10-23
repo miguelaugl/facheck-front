ff4f6d5cb3a876290f8b1d415c8c565df85667e4
Miguel <miguelaugl@outlook.com>
2021-10-23 02:55:44 -0300

    test: ensure AddMonitoringModal calls AddMonitoring with correct values

--- src/presentation/components/add-monitoring-modal/add-monitoring-modal.spec.tsx
+++ src/presentation/components/add-monitoring-modal/add-monitoring-modal.spec.tsx
@@ -1,13 +1,29 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import faker from 'faker'
+import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 
+import { mockAccountModel, mockAddMonitoringParams } from '@/domain/tests'
 import { validationMessages } from '@/presentation/config/yup'
+import { ApiContext, UseCasesContext } from '@/presentation/contexts'
+import { AddMonitoringSpy, simulateFieldInteraction } from '@/presentation/tests'
 
 import { AddMonitoringModal } from './add-monitoring-modal'
 
-const makeSut = (): void => {
+type SutTypes = {
+  addMonitoringSpy: AddMonitoringSpy
+}
+
+const makeSut = (account = mockAccountModel()): SutTypes => {
+  const addMonitoringSpy = new AddMonitoringSpy()
   const onClose = jest.fn()
-  render(<AddMonitoringModal isOpen onClose={onClose} />)
+  render(
+    <UseCasesContext.Provider value={{ addMonitoring: addMonitoringSpy }}>
+      <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+        <AddMonitoringModal isOpen onClose={onClose} />
+      </ApiContext.Provider>
+    </UseCasesContext.Provider>,
+  )
+  return {
+    addMonitoringSpy,
+  }
 }
 
 describe('AddMonitoringModal Component', () => {
@@ -26,36 +42,57 @@ describe('AddMonitoringModal Component', () => {
   it('should show subject validation error', async () => {
     makeSut()
     const subjectInput = screen.getByTestId('subject')
-    fireEvent.blur(subjectInput)
-    expect(await screen.findByTestId('subject-error-message')).toHaveTextContent(validationMessages.mixed.required as string)
+    await waitFor(() => {
+      fireEvent.blur(subjectInput)
+    })
+    expect(screen.getByTestId('subject-error-message')).toHaveTextContent(validationMessages.mixed.required as string)
   })
 
-  it('should show initHour validation error', async () => {
+  it('should show initHour required validation error', async () => {
     makeSut()
     const initHourInput = screen.getByTestId('initHour')
-    fireEvent.blur(initHourInput)
-    expect(await screen.findByTestId('initHour-error-message')).toHaveTextContent(validationMessages.mixed.required as string)
-    fireEvent.input(initHourInput, { target: { value: '32:40' } })
-    expect(await screen.findByTestId('initHour-error-message')).toHaveTextContent(validationMessages.militaryTime)
+    await waitFor(() => {
+      fireEvent.blur(initHourInput)
+    })
+    expect(screen.getByTestId('initHour-error-message')).toHaveTextContent(validationMessages.mixed.required as string)
   })
 
-  it('should show endHour validation error', async () => {
+  it('should show endHour required validation error', async () => {
     makeSut()
     const endHourInput = screen.getByTestId('endHour')
-    fireEvent.blur(endHourInput)
-    expect(await screen.findByTestId('endHour-error-message')).toHaveTextContent(validationMessages.mixed.required as string)
-    fireEvent.input(endHourInput, { target: { value: '32:40' } })
-    expect(await screen.findByTestId('endHour-error-message')).toHaveTextContent(validationMessages.militaryTime)
+    await waitFor(() => {
+      fireEvent.blur(endHourInput)
+    })
+    expect(screen.getByTestId('endHour-error-message')).toHaveTextContent(validationMessages.mixed.required as string)
   })
 
   it('should show room validation error', async () => {
     makeSut()
     const roomInput = screen.getByTestId('room')
-    fireEvent.blur(roomInput)
-    expect(await screen.findByTestId('room-error-message')).toHaveTextContent(validationMessages.mixed.required as string)
-    const roomMaxLength = '50'
-    const maxLengthMessage = (validationMessages.string.max as string).replace(/\${max}/, roomMaxLength)
-    fireEvent.input(roomInput, { target: { value: faker.lorem.lines(30) } })
-    expect(await screen.findByTestId('room-error-message')).toHaveTextContent(maxLengthMessage)
+    await waitFor(() => {
+      fireEvent.blur(roomInput)
+    })
+    expect(screen.getByTestId('room-error-message')).toHaveTextContent(validationMessages.mixed.required as string)
+  })
+
+  it('should call AddMonitoring with correct values', async () => {
+    const addMonitoringParams = mockAddMonitoringParams()
+    const { addMonitoringSpy } = makeSut({
+      ...mockAccountModel(),
+      id: addMonitoringParams.monitorId,
+    })
+    await simulateFieldInteraction('subject', addMonitoringParams.subject)
+    await simulateFieldInteraction('initHour', addMonitoringParams.initHour)
+    await simulateFieldInteraction('endHour', addMonitoringParams.endHour)
+    await simulateFieldInteraction('room', addMonitoringParams.room)
+    const weekdayBtns = screen.getAllByTestId('weekday-btn')
+    await waitFor(() => {
+      fireEvent.click(weekdayBtns[addMonitoringParams.weekday])
+    })
+    act(() => {
+      fireEvent.click(screen.getByText(/adicionar$/i))
+    })
+    await waitFor(() => addMonitoringSpy.add)
+    expect(addMonitoringSpy.params).toEqual(addMonitoringParams)
   })
 })
