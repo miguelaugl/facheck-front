32872603da634497dd5fe1d3742857e108360b4a
Miguel <miguelaugl@outlook.com>
2021-10-23 01:53:20 -0300

    feat: ensure RemoteAddAccount throws UnexpectedError if HttpClient returns 400, 500 or 404

--- src/data/usecases/remote-add-monitoring.ts
+++ src/data/usecases/remote-add-monitoring.ts
@@ -1,14 +1,18 @@
-import { HttpClient } from '@/data/protocols'
+import { HttpClient, HttpStatusCode } from '@/data/protocols'
+import { UnexpectedError } from '@/domain/errors'
 import { AddMonitoring } from '@/domain/usecases'
 
 export class RemoteAddMonitoring implements AddMonitoring {
   constructor(private readonly url: string, private readonly httpClient: HttpClient<void>) {}
 
   async add(params: AddMonitoring.Params): Promise<void> {
-    await this.httpClient.request({
+    const httpResponse = await this.httpClient.request({
       url: this.url,
       method: 'POST',
       body: params,
     })
+    if (httpResponse.statusCode !== HttpStatusCode.SUCCESS) {
+      throw new UnexpectedError()
+    }
   }
 }
