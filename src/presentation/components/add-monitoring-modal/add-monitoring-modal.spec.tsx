53484540f73d9d6de10d805c67257055e75d66b2
Miguel <miguelaugl@outlook.com>
2021-10-23 00:31:03 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/add-monitoring-modal/add-monitoring-modal.spec.tsx
+++ src/presentation/components/add-monitoring-modal/add-monitoring-modal.spec.tsx
@@ -2,10 +2,14 @@ import { render, screen } from '@testing-library/react'
 
 import { AddMonitoringModal } from './add-monitoring-modal'
 
+const makeSut = (): void => {
+  const onClose = jest.fn()
+  render(<AddMonitoringModal isOpen onClose={onClose} />)
+}
+
 describe('AddMonitoringModal Component', () => {
   it('should start with correct initial state', () => {
-    const onClose = jest.fn()
-    render(<AddMonitoringModal isOpen onClose={onClose} />)
+    makeSut()
     expect(screen.getByText(/adicionar$/i)).toBeDisabled()
     expect(screen.getByTestId('subject')).not.toHaveValue()
     const weekdayBtns = screen.getAllByTestId('weekday-btn')
